3e459fc15606a734afaf77058869627df211a4e6
Janani334 <you@example.com>
2026-09-07 05:23:40 +0530

    Learnt variables,Data types,f string, operators

---
+++ Python/Foundations/prgm1.py
@@ -0,0 +1,27 @@
+print("Hellow World!")
+print ('"Hello world"')
+print("'Hellow World!'")
+print('"my name is janani"')
+print(8%2)
+print(8//2)
+print(8/2)
+# print all in a single line
+print(8%7,8/7,8//7)
+# print all in new line so we are using \n
+print('Name:Janu \nAge:22 \nStream:B.Sc')
+Name='Janu'
+Age=22
+Roll_No='101'
+# or use another format
+rollNo='101'
+print("Name")
+# in variables don't mention quotes to display
+print(Name)
+# to print 5 students profile
+print('name age degree roll_no gender')
+print('janu 22 m.sc 102 female')
+name2=input("enter your name:")
+print("The name of second student is: ",name2)
+# another method insteading of using comma
+print(f"the name of second student is: {name2} and the age of first student is: {Age}")
+
